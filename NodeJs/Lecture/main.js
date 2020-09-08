253fb07309788bda8d842930c8bda64d1af6652d
Yee-Kun Jung <48750313+Twoketchupplz@users.noreply.github.com>
2020-09-08 19:30:44 +0900

    학습코드

--- NodeJs/Lecture/main.js
+++ NodeJs/Lecture/main.js
@@ -1,17 +1,18 @@
 var http = require('http');
-var fs = require('fs');
+var url = require('url');
+
 var app = http.createServer(function(request,response){
-    var url = request.url;
-    if(request.url == '/'){
-      url = '/index.html';
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query; //쿼리 데이터 얻는 방법
+    console.log(queryData.id);//주소상 ?id=...에 해당함
+    if(_url == '/'){
+        _url = '/index.html';
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;    }
+    if(_url == '/favicon.ico'){
+        return response.writeHead(404);
+    }
     response.writeHead(200);
-    console.log(__dirname + url);
-    response.end(fs.readFileSync(__dirname + url));
- 
+    response.end(queryData.id);
+
 });
 app.listen(3000);
